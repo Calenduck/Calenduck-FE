99c65b00a2ddafc2d864467fec6f1c6a24b052f6
Tkfrnfl <rul64@naver.com>
2023-06-17 01:31:12 +0900

    add mainpage wire frame

--- src/component/loginModal.tsx
+++ src/component/loginModal.tsx
@@ -2,10 +2,35 @@ import React ,{useEffect,useState}from 'react';
 import '../css/modal.css'
 import Modal from 'react-modal';
 
+
+// const kakaoLogin=()=>{
+
+//     const Rest_api_key='REST API KEY' //REST API KEY
+//     const redirect_uri = 'http://localhost:3000/auth' //Redirect URI
+//     // oauth 요청 URL
+//     const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${'b0eb227d20bd3e34f8503571dbf24772'}&redirect_uri=${redirect_uri}&response_type=code`
+//     const handleLogin = ()=>{
+//         window.location.href = kakaoURL
+//     }
+//     handleLogin()
+//     const code = new URL(window.location.href).searchParams.get("code");
+//     console.log(code)
+// }
+
 const loginModal=(props:any)=> {
   
     const{open,close}=props
+    const Rest_api_key='REST API KEY' //REST API KEY
+    //const redirect_uri = 'http://localhost:8080/users/kakao/login' //Redirect URI
+    const redirect_uri = 'http://localhost:3000/auth' //Redirect URI
 
+    // oauth 요청 URL
+    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=b0eb227d20bd3e34f8503571dbf24772&redirect_uri=${redirect_uri}&response_type=code`
+    const handleKakaoLogin = ()=>{
+        window.location.href = kakaoURL
+    }
+    const code = new URL(window.location.href).searchParams.get("code");
+    console.log(code)
 
     return (
         <div >
@@ -16,7 +41,12 @@ const loginModal=(props:any)=> {
           <div>
             로그인 모달
           </div>
-          
+          <button onClick={handleKakaoLogin}>
+            카카오 로그인
+          </button>
+          <button >
+            구글 로그인
+          </button>
           </Modal>
         </div>
     );
--- src/component/mainPage.tsx
+++ src/component/mainPage.tsx
@@ -5,6 +5,12 @@ import { useSelector, useDispatch } from "react-redux";
 import { getApi } from '../redux/actions';
 import { RootState } from '../redux/reducer';
 import LoginModal from './loginModal';
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import NativeSelect from "@mui/material/NativeSelect";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+import "../css/index.css";
 
 export default () => {
     const dispatch = useDispatch()
@@ -24,14 +30,67 @@ export default () => {
       setModalLoginOpen(false);
     }
 
+    const tmpList=["1","2","3"]
+    const infoListMap=tmpList.map((key)=>
+      (<div key={key} className='item_style'>
+        {key}
+        </div>
+        )
+    )
+
     return(
-        <div className="users">
+        <div >
        
           <div >Test </div>
           <button onClick={showLoginModal}>login</button>
          
+         <div className="center_aligin">
           <LoginModal open={modalLoginOpen} close={closeLoginModal}/>
-         
+          <Box
+          component="form"
+          sx={{
+              '& .MuiTextField-root': { m: 1, width: '100ch' },
+          }}
+          noValidate
+          autoComplete="off"
+          >
+          
+          <FormControl sx={{ m: 1, minWidth: 20 }}>
+              {/* <NativeSelect
+                  defaultValue={"none"}
+                  inputProps={{
+                      name: 'category',
+                      id: 'uncontrolled-native',
+                  }}
+              >
+                  <option value={"none"}>통합검색</option>
+                  <option value={"title"}>제목</option>
+                  <option value={"publisher"}>출판사</option>
+                  <option value={"chapter"}>단원</option>
+              </NativeSelect> */}
+            </FormControl>
+            <SearchIcon />
+              <TextField
+                  id="standard-search"
+                    type="search"
+                    variant="standard"
+              />
+            </Box>
+            <div className='spacer'
+            ></div>
+            <div className='title_sub center-align'>
+              장르
+            </div>
+            <div className='flex'>
+              {infoListMap}
+            </div>
+            <div className='title_sub center-align'>
+              장르
+            </div>
+            <div className='flex'>
+              {infoListMap}
+            </div>
+         </div>
           
       </div>
     )
